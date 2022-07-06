50d54d7a8b56dd225c5314269f49b9bc4538bdbe
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2022-07-06 21:57:17 +0300

    add simple store example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+  console.log(cash);
+
+  const addCash = (cash) => {
+    dispatch({type:"ADD_CASH", payload: cash});
+  }
+
+  const getCash = (cash) => {
+    dispatch({type:"GET_CASH", payload: cash});
+  }
+
   return (
-    <div>     
-          My app
+    <div>   
+      <p>{cash}</p>  
+          <button onClick={() => addCash(Number(prompt()))}>Пововнить</button>
+          <button onClick={() => getCash(Number(prompt()))}>Зняти гроші</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const defaultState = {
+  cash: 100,
+};
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADD_CASH":
+		return {...state, cash: state.cash + action.payload}
+    case "GET_CASH":
+		return {...state, cash: state.cash - action.payload}
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+	<Provider store={store}>
+		<App />
+	</Provider>    
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
