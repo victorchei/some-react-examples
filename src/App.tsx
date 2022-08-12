0207f92d2bfad12d9b7a8171b1e006656406a9ab
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2022-08-12 07:28:08 +0300

    some test ts code2

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function App() {
+  const [state, setState] = useState();
+  return (
+    <div>     
+          My app
+    </div>
+  );
+}
+
+export default App;
