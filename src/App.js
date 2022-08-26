c9d24e72abd5a5ac629ef7b27138cba7e8537895
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2022-08-26 17:34:03 +0300

    add first example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,95 @@
+import { useState } from 'react';
+import { 
+  Button,
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  CssBaseline,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Divider,  
+  ListItemIcon,
+  Hidden,
+} from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 function App() {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <div>     
-          My app
-    </div>
+    <>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Hidden only={['lg', 'xl']}>
+              <IconButton
+                onClick={() => setDrawerOpen(true)}
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          anchor='left'
+          open={isDrawerOpen}
+          onClose={() => setDrawerOpen(false)}
+        >
+          {list()}
+        </Drawer>
+      </Box>
+    </>
   );
 }
 
