2fdab7384d9c8edd9ae56ecf87b2e3039738fbfe
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2022-09-01 16:49:38 +0300

    page-home: add grid

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Box, Grid, Paper } from '@mui/material'
 
 const Home = () => {
-  return <div>home</div>
+  return (
+    <Box sx={{ flexGrow: 1, marginTop: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Paper elevation={2}>Filter Section</Paper>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Paper elevation={2} sx={{ height: '200vh' }}>
+            List of movies
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Paper
+            elevation={2}
+            sx={{ height: '80vh', position: 'sticky', top: '12vh' }}
+          >
+            Selected movies
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 export default Home
